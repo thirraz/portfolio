726e8ef35c2ea4b8b883396b14b7da002643a2ac
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-09-09 12:14:13 -0300

    update: about me section text

--- src/components/about-me-section/AboutMeSection.tsx
+++ src/components/about-me-section/AboutMeSection.tsx
@@ -4,7 +4,8 @@ function AboutMeSection() {
 	return (
 		<AnimatedBorder>
 			<h2 className="font-serif font-semibold leading-[3.3rem] md:leading-[3.5rem] text-[clamp(2rem,7vw,3rem)]">
-				Hi! I’m Thiago, a Front-end developoer located in São Paulo, Brazil.
+				Hi! I&apos;m Thiago, a Front-end developer located in São Paulo,
+				Brazil.
 			</h2>
 			<p className="text-2xl clamp(1.8rem,4vw,2rem)">
 				With 2 years of experience, I studied and worked to create intuitive
