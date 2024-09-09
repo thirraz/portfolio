4936287f0b617692630e163674e6c38b1af87c74
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-09-09 12:08:05 -0300

    update: ContentContainer responsiveness

--- src/components/containers/ContentContainer.tsx
+++ src/components/containers/ContentContainer.tsx
@@ -3,9 +3,8 @@ type Props = { children: React.ReactNode }
 function ContentContainer({ children }: Props) {
 	return (
 		<>
-			<div className="px-3 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-[1200px] mx-auto">
-				{children}
-			</div>
+			{/* <div className="px-3 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-[1200px] mx-auto"> */}
+			<div className="px-3 max-w-[1200px] mx-auto">{children}</div>
 		</>
 	)
 }
