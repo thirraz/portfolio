dcf210f3af6eb4611378ad75f748d6e006d21526
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-09-16 07:42:58 -0300

    updatre: removing selection and pointer events in profile pic

--- src/components/profile-section/ProfilePicComponent.tsx
+++ src/components/profile-section/ProfilePicComponent.tsx
@@ -3,7 +3,7 @@ import profilePic from "../../assets/imgs/profile_pic.jpeg"
 function ProfilePicComponent() {
 	return (
 		<img
-			className="relative rounded-full object-cover w-56 h-56 place-self-center row-start-1 md:col-start-3 md:row-start-2"
+			className="relative rounded-full object-cover w-56 h-56 place-self-center row-start-1 md:col-start-3 md:row-start-2 select-none pointer-events-none"
 			src={profilePic}
 			alt="Thiago Souza's profile pic"
 		/>
