4b1e8f1e9bd65e625462f245fc35cc7ddd41f488
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-09-23 12:31:04 -0300

    update: toggle language button

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,10 @@ import ContentContainer from "./components/containers/ContentContainer"
 import HeadingComponent from "./components/profile-section/HeadingComponent"
 import ProfileGrid from "./components/profile-section/ProfileGrid"
 import ProjectsGrid from "./components/projects-section/ProjectsGrid"
-import { useLanguageContext } from "./contexts/LanguageContext"
 
 function App() {
-	const { language, setLanguage } = useLanguageContext()
-
-	const handleLangChanging = () =>
-		setLanguage(currLang => (currLang == "en" ? "pt-BR" : "en"))
-
-	console.log(language)
-
 	return (
 		<div className="min-h-dvh bg-gradient-to-b from-purple-950 to-purple-900 font-sans">
-			{/* <LanguageSelector /> */}
-			<button
-				onClick={handleLangChanging}
-				className="fixed top-2 right-4 z-50 bg-purple-500 text-white font-bold px-7 py-4 rounded-lg"
-			>
-				Change Language
-			</button>
 			<ContentContainer>
 				<HeadingComponent />
 				<ProfileGrid />
--- src/components/profile-section/HeadingComponent.tsx
+++ src/components/profile-section/HeadingComponent.tsx
@@ -1,11 +1,40 @@
+import { twMerge } from "tailwind-merge"
+import { useLanguageContext } from "../../contexts/LanguageContext"
+
 import Skills from "./SkillsComponent"
+import BRFlag from "../../assets/imgs/brazil_flag.png"
+import USAFlag from "../../assets/imgs/usa_flag.png"
 
 function HeadingComponent() {
+	const { language, setLanguage } = useLanguageContext()
+	const className = language == "pt-BR" ? "left-0" : "right-0"
+
+	const handleLangChanging = () =>
+		setLanguage(currLang => (currLang == "en" ? "pt-BR" : "en"))
+
 	return (
 		<div className="text-white mb-8 space-y-8 md:space-y-0">
-			<h1 className="font-serif text-[clamp(2rem,10vw,4rem)] font-semibold italic">
-				Thiago Morais
-			</h1>
+			<div className="flex items-center justify-between">
+				<h1 className="font-serif text-[clamp(2rem,10vw,4rem)] font-semibold italic">
+					Thiago Morais
+				</h1>
+
+				<div className="flex items-center justify-around  w-48 ">
+					<img src={BRFlag} alt="Brazil's flag icon" />
+					<button
+						onClick={handleLangChanging}
+						className="relative bg-[#3d2f5f] text-white font-bold w-14 h-4 rounded-full "
+					>
+						<span
+							className={twMerge(
+								"absolute right-0 -top-1 w-6 h-6 rounded-full bg-white transition-all",
+								className
+							)}
+						/>
+					</button>
+					<img src={USAFlag} alt="USA's flag icon" />
+				</div>
+			</div>
 
 			<Skills />
 		</div>
